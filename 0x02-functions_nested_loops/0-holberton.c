00b5c9d341a9e0c8bb3fbbe36fcb14d8e63aeae0
nourouhichi <1417@holbertonschool.com>
2020-02-11 20:24:54 +0100

    func

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* main - Entry point
+* _putchar function
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+char ch[] = "Holberton";
+for (i = 0; i <= 8; i++)
+{
+_putchar (ch[i]);
+}
+_putchar ('\n');
+return (0);
+}
