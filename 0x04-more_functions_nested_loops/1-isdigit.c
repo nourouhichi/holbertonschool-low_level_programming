65a5e2778a446b114dfd8f3da9b9775d400cbfe4
nourouhichi <1417@holbertonschool.com>
2020-02-13 20:56:16 +0100

    c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,17 @@
 #include "holberton.h"
 /**
-* print_last_digit - check the code for Holberton School students.
+* _isdigit - check the code for Holberton School students.
 * @c : variable
 * Return: same variable as input.
 */
 int _isdigit(int c)
 {
-if ((c >= 0) && (c <= 9))
-return(1);
-return(0);
+if ((c >= '0') && (c <= '9'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
