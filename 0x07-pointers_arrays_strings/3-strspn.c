3975c54b91ad2876b14a24b729a6354564c9d0fc
nourouhichi <1417@holbertonschool.com>
2020-03-05 23:44:21 +0000

    fun

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,15 +7,15 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-  unsigned int n = 0;
-  while (*s != '\0')
-    {
-      if ((*accept != '\0') && (*s == *accept))
-	{
-	  n++;
-	  accept++;
-	}
-     s++;
-    }
-  return (n);
+unsigned int n = 0;
+while (*s != '\0')
+{
+if ((*accept != '\0') && (*s == *accept))
+{
+n++;
+accept++;
+}
+s++;
+}
+return (n);
 }
