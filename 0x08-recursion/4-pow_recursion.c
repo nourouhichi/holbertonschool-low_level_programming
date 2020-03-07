b9844950c3d7ca890b0f7a54d539bcd29e6b9a37
nourouhichi <1417@holbertonschool.com>
2020-03-07 23:06:58 +0100

    c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 /**
- */
+ * _pow_recursion - cheks the code
+ *@x:int
+ *@y:int
+ *Return: int
+*/
 int _pow_recursion(int x, int y)
 {
-if ( y > 0)
+if (y > 0)
 {
-return(x * _pow_recursion(x, y - 1));
+return (x * _pow_recursion(x, y - 1));
 }
 else if (y < 0)
 {
