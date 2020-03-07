c29919fb7c707c2008a9c1bba53f0620789d1e32
nourouhichi <1417@holbertonschool.com>
2020-03-07 10:51:57 +0100

    c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,15 +7,18 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-  unsigned int n = 0;
-  while (*s != '\0')
-    {
-      if ((*accept != '\0') && (*s == *accept))
-	{
-	  n++;
-	  accept++;
-	}
-     s++;
-    }
-  return (n);
+unsigned int n = 0;
+int i,j;
+while(s[j] != ',')
+{
+for (i = 0; accept[i] != '\0'; i++)
+{
+if (s[j] == accept[i])
+{
+n++;
+}
+}
+j++;
+}
+return (n);
 }
