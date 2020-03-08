9c22def29aac56ad4d9f0aa48a3bb81d9568a083
nourouhichi <1417@holbertonschool.com>
2020-03-08 16:10:49 +0100

    arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - cheks 
+ *@argc: number
+ *@argv: array
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+(void)argc;
+return (0);
+}
