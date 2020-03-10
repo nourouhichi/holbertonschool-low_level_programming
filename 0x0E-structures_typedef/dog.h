d6afa13458fa952937f61a4980cc6ba783ff9bbf
nourouhichi <1417@holbertonschool.com>
2020-03-10 23:05:25 +0100

    c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef REX
 #define REX
+/**
+ *struct dog - cheks
+ *@name: a dof name
+ *@age: a dog age
+ *@owner: a dog owner
+ */
 struct dog
 {
 char *name;
 float age;
-  char *owner;
+char *owner;
 };
 #endif
