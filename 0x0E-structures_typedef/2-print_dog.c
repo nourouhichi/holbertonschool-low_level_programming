d97f77bc0ab994fed62f9f9491bad8a3fe157561
nourouhichi <1417@holbertonschool.com>
2020-03-11 00:43:37 +0100

    c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ *print_dog - checks
+ *@d:pointer
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return;
+if(d->name == NULL)
+printf("Name: (nil)\n");
+else
+printf("Name: %s\n", d->name);
+if (d->owner == NULL)
+printf("Owner: (nil)\n");
+else
+printf("Owner: %s\n", d->owner);
+printf("age: %f\n", d->age);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,5 @@ char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 typedef struct dog dog_t;
+void print_dog(struct dog *d);
 #endif
