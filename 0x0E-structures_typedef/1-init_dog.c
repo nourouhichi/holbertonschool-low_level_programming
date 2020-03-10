0413d021674c82418104c3a5e1f933fe50534802
nourouhichi <1417@holbertonschool.com>
2020-03-11 00:16:54 +0100

    c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
  *init_dog - cheks
  *@d:pointer
@@ -8,7 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if(d == NULL)
+if (d == NULL)
 return;
 d->name = name;
 d->age = age;
