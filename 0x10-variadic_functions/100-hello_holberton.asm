982fad340b5d496d359f2fc03e6b8970f472975b
nourouhichi <1417@holbertonschool.com>
2020-03-14 23:43:21 +0100

    asm

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,13 @@
+	section .data
+	message fb "Hello, Holberton", 10
+	section .text
+	global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 10
+	syscall
+	mov rax, 60
+	mov rdi, 0
+	syscall
