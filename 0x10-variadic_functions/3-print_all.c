a2e28aa11b0bd2d1a10e039733696737679273b6
nourouhichi <1417@holbertonschool.com>
2020-03-15 13:16:17 +0100

    printf simulator with printf

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+ *is_a_char - print if char
+ *@p:pointer to args
+ */
+void is_a_char(va_list p)
+{
+  printf("%c", va_arg(p, int));
+}
+/**
+ *is_an_int - print if int
+ *@p:pointer to args
+ */
+void is_an_int(va_list p)
+{
+  printf("%d", va_arg(p, int));
+}
+/**
+ *is_a_float - prints if float
+ *@p:pointer to args
+ */
+void is_a_float(va_list p)
+{
+  printf("%f", va_arg(p, double));
+}
+/**
+ *is_a_string - prints if string
+ *@p:pointer to args
+ */
+void is_a_string(va_list p)
+{
+  char *ptr;
+  ptr = va_arg(p, char*);
+  if (ptr == NULL)
+    {
+      ptr = "(nil)";
+    }
+  printf("%s", ptr);
+}
+/**
+ *print_all - prints anything
+ *@format:a pointer 
+ */
+void print_all(const char * const format, ...)
+{
+  va_list p;
+  int i = 0;
+  char *nothing;
+
+  va_start(p, format);
+  while (format[i] != '\0' && format != NULL)
+    {
+      switch (format[i])
+	{
+	case 'c':
+	  is_a_char(p);
+	  break;
+	case 'i':
+	  is_an_int(p);
+	  break;
+	case 'f':
+	  is_a_float(p);
+	  break;
+	case 's':
+	  is_a_string(p);
+	  break;
+	  printf("%s", nothing);
+	  break;
+	}
+      if ((format[i + 1] != '\0') && (format[i] == 'i' || format[i] == 'c' || format[i] == 's' || format[i] == 'f'))
+	printf(", ");
+      i++;
+    }
+  printf("\n");
+  va_end(p);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
 #ifndef ARGS
 #define ARGS
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void is_a_string(va_list p);
+void is_an_int(va_list p);
+void is_a_float(va_list p);
+void is_a_char(va_list p);
 #endif
