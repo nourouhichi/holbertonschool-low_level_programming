6bb88c2122c0529fa431c0aefd0fa9a44732b297
nourouhichi <1417@holbertonschool.com>
2020-03-15 13:38:22 +0100

    simulator

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,7 +8,7 @@
  */
 void is_a_char(va_list p)
 {
-  printf("%c", va_arg(p, int));
+printf("%c", va_arg(p, int));
 }
 /**
  *is_an_int - print if int
@@ -16,7 +16,7 @@ void is_a_char(va_list p)
  */
 void is_an_int(va_list p)
 {
-  printf("%d", va_arg(p, int));
+printf("%d", va_arg(p, int));
 }
 /**
  *is_a_float - prints if float
@@ -24,7 +24,7 @@ void is_an_int(va_list p)
  */
 void is_a_float(va_list p)
 {
-  printf("%f", va_arg(p, double));
+printf("%f", va_arg(p, double));
 }
 /**
  *is_a_string - prints if string
@@ -32,48 +32,49 @@ void is_a_float(va_list p)
  */
 void is_a_string(va_list p)
 {
-  char *ptr;
-  ptr = va_arg(p, char*);
-  if (ptr == NULL)
-    {
-      ptr = "(nil)";
-    }
-  printf("%s", ptr);
+char *ptr;
+ptr = va_arg(p, char*);
+if (ptr == NULL)
+{
+ptr = "(nil)";
+}
+printf("%s", ptr);
 }
 /**
  *print_all - prints anything
- *@format:a pointer 
+ *@format:a pointer
  */
 void print_all(const char * const format, ...)
 {
-  va_list p;
-  int i = 0;
-  char *nothing;
-
-  va_start(p, format);
-  while (format[i] != '\0' && format != NULL)
-    {
-      switch (format[i])
-	{
-	case 'c':
-	  is_a_char(p);
-	  break;
-	case 'i':
-	  is_an_int(p);
-	  break;
-	case 'f':
-	  is_a_float(p);
-	  break;
-	case 's':
-	  is_a_string(p);
-	  break;
-	  printf("%s", nothing);
-	  break;
-	}
-      if ((format[i + 1] != '\0') && (format[i] == 'i' || format[i] == 'c' || format[i] == 's' || format[i] == 'f'))
-	printf(", ");
-      i++;
-    }
-  printf("\n");
-  va_end(p);
+va_list p;
+int i = 0;
+char x;
+char *nothing;
+va_start(p, format);
+while (format[i] != '\0' && format != NULL)
+{
+switch (format[i])
+{
+case 'c':
+is_a_char(p);
+break;
+case 'i':
+is_an_int(p);
+break;
+case 'f':
+is_a_float(p);
+break;
+case 's':
+is_a_string(p);
+break;
+printf("%s", nothing);
+break;
+}
+x = format[i];
+if ((format[i + 1] != '\0') && (x == 'i' || x == 'c' || x == 's' || x == 'f'))
+printf(", ");
+i++;
+}
+printf("\n");
+va_end(p);
 }
