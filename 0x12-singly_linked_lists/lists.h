f2278a67155e289ba6c5c9f3dab096fa972132e9
nourouhichi <1417@holbertonschool.com>
2020-03-21 17:08:41 +0100

    add lib

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef STRUCT_H
 #define STRUCT_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -11,9 +14,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 int _putchar(char c);
