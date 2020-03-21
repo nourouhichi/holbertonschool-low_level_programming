da68d177ff4a4bf2aff581c704b747769e5614a8
nourouhichi <1417@holbertonschool.com>
2020-03-22 00:58:32 +0100

    attribute constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ *before_main - executes printf before main
+ */
+void before_main(void)
+{
+  printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,4 +25,5 @@ list_t *add_node(list_t **head, const char *str);
 unsigned int size(const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void before_main(void) __attribute__((constructor));
 #endif
