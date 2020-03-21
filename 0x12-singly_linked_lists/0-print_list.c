922a166dcdd71f699cf5908509b61fcbd3b66571
nourouhichi <1417@holbertonschool.com>
2020-03-21 16:52:15 +0100

    task 0 first try

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,20 @@
 #include "lists.h"
 /**
+ *print_list - prints elements of the list
+ *@h:list_t
+ *Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
+size_t i = 0 ;
+while (h != NULL)
+{
+if (h-> str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+i++;
+}
+return (i);
 }
