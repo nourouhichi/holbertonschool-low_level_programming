6bb34483ff1b205b24cddb07985ad491c59a7e53
nourouhichi <1417@holbertonschool.com>
2020-03-24 21:26:04 +0100

    checker issue

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 unsigned int i = 0;
 listint_t *new_node;
 listint_t *road_runner;
-if (!head || *head == NULL)
+if (!head)
 return (NULL);
 new_node = malloc(sizeof(listint_t));
 if (!new_node)
