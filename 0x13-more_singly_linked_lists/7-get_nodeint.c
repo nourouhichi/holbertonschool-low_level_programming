a0dc898fe436e05ff0d96019c6a363e118855b5f
nourouhichi <1417@holbertonschool.com>
2020-03-24 16:29:15 +0100

    inattention

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,8 +7,8 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-unsigned int i;
-while (i > index)
+unsigned int i = 0;
+while (i < index)
 {
 if (head->next == NULL)
 return (NULL);
