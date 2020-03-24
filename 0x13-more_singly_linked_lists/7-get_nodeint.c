d30389d1443ac471b844eb6e1fc9e2a549199d32
nourouhichi <1417@holbertonschool.com>
2020-03-24 16:24:54 +0100

    task 7 first try

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *get_nodeint_at_index - gets the nth node
+ *@head: pointer
+ *@index:n
+ *Return:node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int i;
+while (i > index)
+{
+if (head->next == NULL)
+return (NULL);
+head = head->next;
+i++;
+}
+return (head);
+}
