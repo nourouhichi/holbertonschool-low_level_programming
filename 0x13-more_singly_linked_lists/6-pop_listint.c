1b161a58814e8d3266d3ef243ea336ed2728f480
nourouhichi <1417@holbertonschool.com>
2020-03-24 16:04:09 +0100

    fixing

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,5 +1,8 @@
 #include "lists.h"
 /**
+ *pop_listint - deletes the head
+ *@head:listint_t
+ *Return: int
  */
 int pop_listint(listint_t **head)
 {
@@ -9,6 +12,7 @@ if (*head == NULL)
 return (0);
 head2 = *head;
 n = head2->n;
+*head = head2->next;
 free(head2);
 return (n);
 }
