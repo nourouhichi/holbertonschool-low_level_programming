896a80b93ec0fa41e912c71721de112372aff7b0
nourouhichi <1417@holbertonschool.com>
2020-03-25 21:12:37 +0100

    fixing malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,36 +1,22 @@
 #include "holberton.h"
 /**
- *size - returns the size of a string
- *@str:the string
- *Return:int
- */
-int size(char *str)
-{
-int i;
-if (str == NULL)
-return(0);
-for (i = 0; str[i] != '\0';)
-{
-i++;
-}
-return (i);
-}
-/**
  *_strdup - clones a string
  *@str: a string
  *Return:apointer
  */
 char *_strdup(char *str)
 {
-int i = size(str);
-char *clone = malloc(sizeof(char) * i);
+int i;
+char *clone;
+if (str == NULL)
+return (NULL);
+for (i = 0; str[i] != '\0';)
+{
+i++;
+} 
+clone = (char *) malloc(sizeof(char) * i);
 if (clone == NULL)
 return (NULL);
-if (str == NULL)
-{
-free(clone);
-return (NULL);
-}
 for (i = 0; str[i] != '\0'; i++)
 {
 clone[i] = str[i];
