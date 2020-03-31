ee8dc76c2d5d11f7b53a9c3eb152e455f23bda67
nourouhichi <1417@holbertonschool.com>
2020-04-01 00:20:42 +0100

    another try

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,9 +20,8 @@ fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (fd1 == -1)
 {dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 exit(99); }
-rd = read(fd1, buff, 1024);
 wr = write(fd2, buff, rd);
-if (rd != 0)
+while ((rd = read(fd1, buff, 1024)) != 0)
 {
 if (rd == -1)
 {dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
