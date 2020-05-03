7ff29d79da8a5fbab69ec5ac45b02319878b48c3
nourouhichi <1417@holbertonschool.com>
2020-05-03 22:34:09 +0100

    list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -26,4 +26,5 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+void main(void);
 #endif
