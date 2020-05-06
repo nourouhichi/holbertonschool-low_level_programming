4da0589ddae15321ce3faf5c545acd0b7fbdad1c
nourouhichi <1417@holbertonschool.com>
2020-05-06 15:26:01 +0100

    new try

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O -q /tmp/win.so https://github.com/nourouhichi/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
+curl https://github.com/nourouhichi/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
 
 
