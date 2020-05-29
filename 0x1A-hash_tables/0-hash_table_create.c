612ad8f3a9a034981a3c9bb5d30e1713993525ce
nourouhichi <1417@holbertonschool.com>
2020-05-29 10:35:04 +0100

    new try

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,11 +11,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!table)
 		return (NULL);
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t));
+	table->array = calloc(size, sizeof(hash_node_t *));
 	if (!table->array)
-	{
-		free(table);
 		return (NULL);
-	}
 	return (table);
 }
