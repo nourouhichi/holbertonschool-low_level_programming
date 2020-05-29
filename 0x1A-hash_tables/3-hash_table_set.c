218e10dbbb62e8978e73fed9baf9ebb5a445ccc0
nourouhichi <1417@holbertonschool.com>
2020-05-29 10:41:40 +0100

    new try

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,22 +11,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_hash_node, *roadrunner;
 	unsigned long int index;
 
-	if (!key)
+	if (!key || !*key)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index])
 	{
 		roadrunner = ht->array[index];
-		while (roadrunner && roadrunner->next)
-		{
-			roadrunner = roadrunner->next;
-		}
 		new_hash_node = malloc(sizeof(hash_node_t));
 		if (!new_hash_node)
 			return (0);
+		ht->array[index] = new_hash_node;
+		new_hash_node->next = roadrunner;
 		new_hash_node->key = strdup(key);
 		new_hash_node->value = strdup(value);
-		roadrunner->next = new_hash_node;
 		return (1);
 	}
 	new_hash_node = malloc(sizeof(hash_node_t));
