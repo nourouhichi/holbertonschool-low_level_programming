c51ca5741c2c43b3e3634efea2560c69f49e2de7
nourouhichi <1417@holbertonschool.com>
2020-09-24 23:43:55 +0100

    new add

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,13 +11,16 @@ unsigned int _strspn(char *s, char *accept)
 int j, i;
 unsigned int count = 0;
 
+if (s && accept)
+{	
 for  (i = 0; accept[i] != '\0'; i++ )
 {
-	for (j = 0; s[j] != ' '; j++)
-	{
-		if (s[j] == accept[i])
-			count = count + 1;
-	}
+for (j = 0; s[j] != ' '; j++)
+{
+if (s[j] == accept[i])
+count = count + 1;
+}
+}
 }
 return (count);
 }
