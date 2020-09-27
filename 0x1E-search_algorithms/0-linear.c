5c665a761ae8bb72776fb329b71e16738d39c465
nourouhichi <1417@holbertonschool.com>
2020-09-27 09:12:18 +0100

    new fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,17 +10,17 @@ int linear_search(int *array, size_t size, int value)
 {
 int i, x = size;
 
-if (!*array)
+if (!*array || size == 0)
 return (-1);
 for (i = 0;  i  != x;)
 {
 if (array[i] == value)
 {
-printf("Value checked array[%d] = [%d]\n",i , array[i]);
+printf("Value checked array[%d] = [%d]\n", i, array[i]);
 return (i);
 }
 else
-printf("Value checked array[%d] = [%d]\n",i , array[i]);
+printf("Value checked array[%d] = [%d]\n", i, array[i]);
 i++;
 }
 return (-1);
